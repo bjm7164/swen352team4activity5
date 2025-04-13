a6f191bc3dfe1f6bce3002f2024a7769578f6e78
JT Novitsky <jtn4179@rit.edu>
2025-04-13 17:40:55 -0400

    MyDealer : 1 : constructor : create passing code

--- src/main/java/edu/rit/swen352/tdd/MyBlackjackDealer.java
+++ src/main/java/edu/rit/swen352/tdd/MyBlackjackDealer.java
@@ -2,4 +2,13 @@ package edu.rit.swen352.tdd;
 
 public class MyBlackjackDealer {
 
+    private int deckSize; // representing deck with size for sake of the assignment
+    private int handValue;
+
+    public MyBlackjackDealer()
+    {
+        deckSize = 52;
+        handValue = 0;
+    }
+
 }
