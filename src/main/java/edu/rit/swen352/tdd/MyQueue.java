dc37ba34c39c7aaa45cdbf4c6688b214d917c201
Christopher <cpw9783@rit.edu>
2025-04-13 16:34:48 -0400

    starter code for proposal

---
+++ src/main/java/edu/rit/swen352/tdd/MyQueue.java
@@ -0,0 +1,15 @@
+package edu.rit.swen352.tdd;
+
+/**
+ * MyQueue is a collection that allows adding and removing elements in a first-in-first-out (FIFO) manner.
+ *<ul>
+ *     * <li>methods:
+ *     <ul>
+ *         <li>{@code add(element:T)} -- adds an element to the end of the queue</li>
+ *         <li>{@code remove()} -- removes and returns the first element in the queue; throws {@link java.util.NoSuchElementException} if empty</li>
+ *         <li>{@code peek()} -- returns the first element in the queue without removing it; throws {@link java.util.NoSuchElementException} if empty</li>
+ *     </ul>
+ *</ul>
+ */
+public class MyQueue {
+}
---
+++ src/test/java/edu/rit/swen352/tdd/MyQueueTest.java
@@ -0,0 +1,7 @@
+package edu.rit.swen352.tdd;
+
+/**
+ * Test suite for the {@link MyQueue} component.
+ */
+public class MyQueueTest {
+}
